176642bc959447829e0731aed003cea1e0ea2852
tlilley <lilleyt1@gmail.com>
2017-05-21 17:12:59 -0700

    Test a change

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("This is my first program");
+		System.out.println("This is my second program");
 
 	}
 
